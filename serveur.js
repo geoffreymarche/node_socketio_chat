7fe33f91b79f403a8d0200f527dced7eaab198b3
geoffreymarche <geoffrey.marche@themoocagency.com>
2017-03-21 11:25:23 +0100

    add history

--- serveur.js
+++ serveur.js
@@ -1,4 +1,5 @@
-var app = require('express')(), // utilisation app express
+var express = require('express'),
+    app = express(), // utilisation app express
     server = require('http').createServer(app), // creation du server http
     io = require('socket.io').listen(server), // socket.io
     bodyParser = require('body-parser'), // parser les req http
@@ -10,13 +11,30 @@ app.use(bodyParser.urlencoded({     // encoder en json les param d'url de req ht
   extended: true
 }));
 var channel = '';
-// Chargement de la page index.html
-app.get('/:channel', function (req, res) {
+var username = '';
+app.use(express.static(__dirname + '/views'));
+// Chargement de la page client.html
+app.get('/:channel/:username', function (req, res) {
   channel = ent.encode(req.params.channel);
+  username = ent.encode(req.params.username);
   res.sendfile(__dirname + '/client.html');
 });
+app.get('/api/log/:file',function(req,res) {
+  var file = ent.encode(req.params.file);
+  if(fs.existsSync("assets/log/"+file+".json")) {
+    fs.readFile("assets/log/"+file+".json", function (err, results) {
+        var json = JSON.parse(results);
+        var retour = {reponse:true,data:json};
+        res.send(retour);
+    })
+  }else{
+    var retour = {reponse:false};
+    res.send(retour);
+  }
+})
 io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    /*
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
         // creation du pseudo
@@ -27,31 +45,33 @@ io.sockets.on('connection', function (socket, pseudo) {
         socket.join(channel);
         socket.broadcast.to(channel).emit('nouveau_client', pseudo);
     });
-
+    */
+    // action au log :
+    // creation du pseudo
+    socket.pseudo = username;
+    //creation du cannal
+    socket.room = channel;
+    // rejoindre le cannal
+    socket.join(channel);
+    socket.broadcast.to(channel).emit('nouveau_client', socket.pseudo);
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
         var data = {pseudo: socket.pseudo, message: message};
         // creation des logs
-        if (fs.existsSync("./"+socket.room+".json")) {
+        if (fs.existsSync("assets/log/"+socket.room+".json")) {
           // si fichier log existe deja :
-          fs.readFile("./"+socket.room+".json", function (err, results) {
+          fs.readFile("assets/log/"+socket.room+".json", function (err, results) {
               var json = JSON.parse(results);
               json.message.push(data);
-              fs.writeFile("./"+socket.room+".json", JSON.stringify(json));
+              fs.writeFile("assets/log/"+socket.room+".json", JSON.stringify(json));
           })
         }else {
           // action si non
           var insert = {
             message:[data],
           }
-          fs.writeFile("./"+socket.room+".json", JSON.stringify(insert, null, 4), (err) => {
-              if (err) {
-                  console.error(err);
-                  return;
-              };
-              console.log("File has been created");
-          });
+          fs.writeFile("./"+socket.room+".json", JSON.stringify(insert, null, 4));
         }
         // envoi du message
         io.sockets.to(socket.room).emit('message', data);
